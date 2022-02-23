5a55e1d320b7c852cadea75743d045a525965154
AaronArdz <aaron_acostarz@outlook.es>
2022-02-23 01:05:43 -0600

    added data set creator and reader, implemented solver from google collabs previous code

---
+++ main.py
@@ -0,0 +1,102 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import sys
+
+import numpy as np
+import pandas as pd
+import csv
+import solve_greedy as sv
+
+
+## 1 step, Create columns
+## 2 create cost value
+## 3 iterate through list of columns and select random index
+## 4 save random index number in a dictionary to make sure there's no duplicates
+## 6 adjust the size of the dictionary to fit the specified album length
+def create_album(size, subset):
+    subset_values = subset.flatten()
+    subset_len = len(subset_values)
+    album = dict()
+    while len(album.keys()) <= size - 1:
+        random_index = np.random.randint(subset_len, size=1)[0]
+        value = subset_values[random_index]
+        if value not in album:
+            album[value] = 0
+        else:
+            value = subset_values[random_index - 1]
+            album[value] = 0
+
+    return album
+
+
+def create_subsets(number_of_subsets, columns, rangen):
+    subsets = np.random.randint(rangen, size=(number_of_subsets, columns))
+    return subsets
+
+
+def create_cost_values(number_of_subsets):
+    return np.random.rand(number_of_subsets)
+
+
+def define_dataset(number_of_subsets, columns, album_size, rangen):
+    subs = create_subsets(number_of_subsets, columns, rangen)
+    cost_values = create_cost_values(number_of_subsets)
+    album_list = create_album(album_size, subs)
+
+    subset_with_cost = list()
+
+    for i in range(len(subs)):
+        tup = [subs[i], cost_values[i], i]
+        subset_with_cost.append(tup)
+
+    with open("big2.csv", "w", newline='') as f:
+        writer = csv.writer(f, lineterminator='\n')
+        writer.writerows(map(lambda x: [x], album_list.keys()))
+        writer.writerows(subset_with_cost)
+
+def read_data_set():
+    file1 = open('big2.csv', 'r')
+    lines = file1.readlines()
+    temp_dict = dict()
+    temp_list = list()
+
+    for line in lines:
+        new_line = line.strip().split(',')
+        print(new_line, len(new_line))
+        if len(new_line) == 1:
+            temp_dict[new_line[0]] = 0
+        else:
+            # int_subset = map(lambda x: [x], new_line[0][1:-1].split())
+            # print(int_subset)
+            split_line = [new_line[0][1:-1].split(), float(new_line[1]), int(new_line[2])]
+            temp_list.append(split_line)
+
+    return temp_list, temp_dict
+
+def readd():
+    with open('big_dataset_3.csv', newline='') as f:
+        reader = csv.reader(f)
+        temp_dict = dict()
+        temp_list = list()
+        for line in reader:
+            new_line = line
+            if len(new_line) == 1:
+                temp_dict[new_line[0]] = 0
+            else:
+                # int_subset = map(lambda x: [x], new_line[0][1:-1].split())
+                # print(int_subset)
+                split_line = [new_line[0][1:-1].split(), float(new_line[1]), int(new_line[2])]
+                temp_list.append(split_line)
+    return temp_list, temp_dict
+
+if __name__ == '__main__':
+    # define_dataset(30, 20, 15, 10000)
+    sub, alb = readd()
+    solver = sv.solver(sub, alb)
+    winning_list, total, albs = solver.solve_greedy()
+    print(winning_list)
+    print(total)
+
+
---
+++ solve_greedy.py
@@ -0,0 +1,34 @@
+class solver:
+    subsets = list()
+    album_universe = dict()
+
+    # parameterized constructor
+    def __init__(self, subsets, album_universe):
+        self.subsets = subsets
+        self.album_universe = album_universe
+
+    def solve_greedy(self):
+        total = 0
+        covered_album = set()
+        covered_subsets = set()
+
+        # sort tuples by cost value
+        sorted_subsets = sorted(self.subsets, key=lambda x: x[1])
+
+        # initialize dictionary with values 0
+        print(len(self.album_universe.keys()))
+
+        # select subsets and fill album
+        for subset in sorted_subsets:
+            index = subset[2]
+            if len(covered_album) == len(self.album_universe.keys()):
+                print(len(covered_album), len(self.album_universe.keys()), 'All covered')
+                break
+            for j in subset[0]:
+                if j in self.album_universe and self.album_universe.get(j) != 1:
+                    self.album_universe[j] = 1
+                    covered_album.add(j)
+                    covered_subsets.add(index)
+                    total += subset[1]
+
+        return covered_subsets, total, self.album_universe
